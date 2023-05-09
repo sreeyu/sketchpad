94409ece18bedd2b33f93596581d0553830491d4
sreey <sreelakshmi12898@gmail.com>
2023-05-09 12:56:00 +0530

    Add color to square on hover

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const container = document.querySelector('.container');
 
 for (let i = 0; i < 256; i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+    const squares = document.createElement('div');
+    squares.classList.add('square');
+    container.appendChild(squares);
 }
+
+function setBg(square){
+    square.style.backgroundColor = 'yellow';
+}
+
+container.addEventListener('mouseover', function(e){
+
+    if (e.target.classList.contains('square')){
+        setBg(e.target)
+    }
+});
