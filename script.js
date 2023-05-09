1841a06212f3d2122dcbdb49e2715af2fcad2702
sreey <sreelakshmi12898@gmail.com>
2023-05-09 22:23:09 +0530

    Add popup to take user input

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 
+
 for (let i = 0; i < 256; i++){
     const squares = document.createElement('div');
     squares.classList.add('square');
@@ -16,3 +17,47 @@ container.addEventListener('mouseover', function(e){
         setBg(e.target)
     }
 });
+
+const popBtn = document.getElementById('btn');
+popBtn.addEventListener('click', function(){
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    popup.innerHTML = `<p>Enter the number of squares you want in a grid:</p>
+                       <input type='number' />
+                       <div class="btn"><button id="ok-btn">Okay</button>
+                       <button id="close-btn">Close</button></div>`;
+    document.body.appendChild(popup);
+
+    const okBtn = popup.querySelector('#ok-btn');
+    const input = popup.querySelector('input');
+
+    okBtn.disabled = true;
+    
+    input.addEventListener('input', () =>{
+        if(input.value){
+            okBtn.disabled = false;
+            okBtn.addEventListener('click', () =>{
+                const gridNum = input.value;
+                console.log(gridNum);
+            })
+        }
+        else{
+            okBtn.disabled = true;
+        }
+       
+    })
+    
+})
+
+
+
+function updateGrid(n){
+    const square = document.querySelectorAll('.container .square');
+    square.forEach(squares => {
+        console.log(n);
+      squares.style.width = "20px";
+   })
+}
+
+
+
