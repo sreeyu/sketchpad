ca3f72b72b8f41364b93c8dcba671f4f41854638
sreey <sreelakshmi12898@gmail.com>
2023-05-17 20:26:13 +0530

    Add a range input to get number of grid

--- script.js
+++ script.js
@@ -18,83 +18,31 @@ container.addEventListener('mouseover', function(e){
     }
 });
 
-const popBtn = document.getElementById('btn');
-popBtn.addEventListener('click', function(){
-    const popup = document.createElement('div');
-    popup.classList.add('popup');
-    popup.innerHTML = `<p>Enter the number of squares you want in a grid:</p>
-                       <input type='number' />
-                       <div class="btn"><button id="ok-btn">Okay</button>
-                       <button id="close-btn">Close</button></div>`;
-    document.body.appendChild(popup);
-
-    const okBtn = popup.querySelector('#ok-btn');
-    const input = popup.querySelector('input');
-
-    okBtn.disabled = true;
-    
-    input.addEventListener('input', () =>{
-        if(input.value){
-            okBtn.disabled = false;
-        }
-        else{
-            okBtn.disabled = true;
-        }
-       
-    });
-
-    okBtn.addEventListener('click', () =>{
-        const gridNum = input.value;
-        popup.remove();
-        updateGrid(gridNum);
-    })
-
-    const cancel = popup.querySelector('#close-btn');
-    cancel.addEventListener('click', ()=>{
-        popup.remove();
-    })
-
-    
-})
-
-
-
-function updateGrid(gridNum){
-
+const createGrid = (gridNum) => {
     container.style.setProperty('--grid-rows', gridNum);
     container.style.setProperty('--grid-cols', gridNum);
 
     for (i = 0; i < (gridNum * gridNum); i++){
         const squares = document.createElement('div');
         squares.classList.add('square');
-        
+            
         container.appendChild(squares);
     }
-
-    /*const oldGrid = document.querySelectorAll('.container .square');
-    oldGrid.forEach(square => {
-        square.remove();
-    })
-    
-    const n = Number(gridNum);
-
-    for (let i = 0; i < n*n; i++){
-        const squares = document.createElement('div');
-        squares.classList.add('square');
-        container.appendChild(squares);
-    }
-    
-    const width = Math.round((586/n) - 2);
-    const height = width + 0.2;
-    console.log(container);
-
-    const squares = document.querySelectorAll('.container .square');
-    squares.forEach(square => {
-      square.style.width = `${width}px`;
-      square.style.height = `${height}px`;
-   }) */
-   
+    document.getElementById('rangeValue').innerText = gridNum;
 }
 
 
+ createGrid(document.getElementById('slider').value);
+
+
+document.getElementById('slider').addEventListener('input',() => {
+
+    const oldGrid = document.querySelectorAll('.container .square');
+    oldGrid.forEach(square => {
+        square.remove();
+    })
+
+    const x = document.getElementById('slider').value;
+    createGrid(x);
+})
 
